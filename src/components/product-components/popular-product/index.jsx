b89a21ca043d3966bdf6dd51d9d118f2f4080e94
uzxakker <103135051+uzxakker@users.noreply.github.com>
2023-05-25 23:18:23 +0500

    push

--- src/components/product-components/popular-product/index.jsx
+++ src/components/product-components/popular-product/index.jsx
@@ -8,6 +8,7 @@ import { NavLink, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { ProductGet } from "../../../redux/product";
 import { CategoryGet } from "../../../redux/category";
+import { InboxOutlined } from "@ant-design/icons";
 
 const PopularProduct = () => {
   const { id } = useParams();
@@ -24,7 +25,7 @@ const PopularProduct = () => {
   const categoryGets = useSelector((state) => state.category.categoryGet.data);
   const filterCategory = productGets.filter((elem) => elem.category.id == id);
   console.log(
-    filterCategory.map((product) => product.category.category_name_ru)
+    filterCategory
     // productGets.filter(elem => elem.category.id == id)
   );
   const LangVal = () => {
@@ -36,45 +37,54 @@ const PopularProduct = () => {
       <div className={styles.popular_section}>
         <BigContainer>
           <div className={styles.row_wrap}>
-            <div className={styles.choose_title}>
-              {filterCategory.map((e) => (
-                <h2>
-                  {LangVal() == "ru"
-                    ? e.category.category_name_ru
-                    : LangVal() == "uz"
-                    ? e.category.category_name_uz
-                    : e.category.category_name_ru}
-                </h2>
-              ))}
+            {filterCategory ? (
+              <>
+                <div className={styles.choose_title}>
+                  {filterCategory.map((e) => (
+                    <h2>
+                      {LangVal() == "ru"
+                        ? e.category.category_name_ru
+                        : LangVal() == "uz"
+                        ? e.category.category_name_uz
+                        : e.category.category_name_ru}
+                    </h2>
+                  ))}
 
-              <div className={styles.line}></div>
-            </div>
-            <Row className={styles.row}>
-              {filterCategory.map((product) => (
-                <Col className={styles.col} lg={4} md={6} sm={12} sx={12}>
-                  <div className={styles.product_card}>
-                    <NavLink
-                      className={styles.params_link}
-                      to={`/productmore/${product.id}`}
-                    >
-                      <img
-                        src={product.images
-                          .slice(0, 1)
-                          .map((imgs) => imgs.image)}
-                        alt=""
-                      />
-                      <h4>
-                        {LangVal() == "ru"
-                          ? product.title_ru
-                          : LangVal() == "uz"
-                          ? product.title_uz
-                          : product.title_ru}
-                      </h4>
-                    </NavLink>
-                  </div>
-                </Col>
-              ))}
-            </Row>
+                  <div className={styles.line}></div>
+                </div>
+                <Row className={styles.row}>
+                  {filterCategory.map((product) => (
+                    <Col className={styles.col} lg={4} md={6} sm={12} sx={12}>
+                      <div className={styles.product_card}>
+                        <NavLink
+                          className={styles.params_link}
+                          to={`/productmore/${product.id}`}
+                        >
+                          <img
+                            src={product.images
+                              .slice(0, 1)
+                              .map((imgs) => imgs.image)}
+                            alt=""
+                          />
+                          <h4>
+                            {LangVal() == "ru"
+                              ? product.title_ru
+                              : LangVal() == "uz"
+                              ? product.title_uz
+                              : product.title_ru}
+                          </h4>
+                        </NavLink>
+                      </div>
+                    </Col>
+                  ))}
+                </Row>
+              </>
+            ) : (
+              <div className={styles.no_data_box}>
+                <InboxOutlined className={styles.ant_icons} />
+                <h4>{t("Products.10")}</h4>
+              </div>
+            )}
           </div>
         </BigContainer>
       </div>
